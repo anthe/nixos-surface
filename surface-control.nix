e92c22710cbb70edcd76c525b868ef79a3d8bf64
anthe <github@halcyon.nu>
2020-05-21 11:02:06 -0700

    updated cargo hash for surface-control

--- surface-control.nix
+++ surface-control.nix
@@ -6,6 +6,6 @@ rustPlatform.buildRustPackage rec {
     url = "https://github.com/qzed/linux-surface-control/archive/v${version}.tar.gz";
     sha256 = "0llimw0xjf2agr1m3f46c9db87wpxjpdm6300l6sgc28vs03cjkg";
   };
-
+  
   cargoSha256 = "0zq0nqlvzvgzrgr3cx5gbd2x2n3pdzxp1xsy6jhkyk8ap4cr9jp9";
 }
